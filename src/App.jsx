a1bcdd6a4bc44d2da08ab41ffc06ea6647636c6a
guustavo07 <gustavoo.go59@gmail.com>
2025-06-24 07:38:23 -0300

    Refatorando para alguns componentes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,22 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+
+      setTasks(data);
+    };
+    //Caso queira usar os dados da api
+    //fetchTasks();
+  }, []);
+
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       if (task.id === taskId) {
